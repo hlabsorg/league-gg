e890c109ad977c8ddd6e9261888d781e839b0358
Aukai Tirrell <aukaitirrell@webmocha.com>
2024-11-15 14:35:50 -0700

    update layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -23,7 +23,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+      <body className={cn(`${geistSans.variable} ${geistMono.variable} antialiased`)}>{children}</body>
     </html>
   );
 }
