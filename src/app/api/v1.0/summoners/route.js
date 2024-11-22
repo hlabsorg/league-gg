06c90ad4e55a261faba985fe5e2cb723d5eae8d0
Aukai Tirrell <aukaitirrell@webmocha.com>
2024-11-21 18:24:23 -0700

    fix query params for summoners route

--- src/app/api/v1.0/summoners/route.js
+++ src/app/api/v1.0/summoners/route.js
@@ -2,12 +2,18 @@ import { NextResponse } from "next/server";
 import { getRiotAccount } from "@/lib/server/riot";
 
 export const GET = async (req) => {
-  const { gameName, tagLine, regionId } = await req.query;
+  const { searchParams } = req.nextUrl;
+  const gameName = searchParams.get("gameName");
+  const tagLine = searchParams.get("tagLine");
+  const regionId = searchParams.get("regionId");
+  if (!gameName || !tagLine || !regionId) {
+    return NextResponse.json({ error: "Missing required parameter" }, { status: 400 });
+  }
   try {
     const riotAccount = await getRiotAccount(gameName, tagLine, regionId);
     return NextResponse.json(riotAccount);
   } catch (error) {
     console.error(error);
-    return NextResponse.json({ error: error.message }, { status: error.code, statusText: error.message });
+    return NextResponse.json({ error: error.message }, { status: error.status });
   }
 };
