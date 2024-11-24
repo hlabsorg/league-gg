074d2fa3e20d0dd9a74c2e35e2e9bf59301aa89a
Aukai Tirrell <aukaitirrell@webmocha.com>
2024-11-24 09:12:55 -0700

    changes to images

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "seukqkukqze4vjj1.public.blob.vercel-storage.com",
+        port: "",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
