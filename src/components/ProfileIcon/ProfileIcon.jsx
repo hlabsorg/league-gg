bae21df3b5fd4d3c6a859de57fcf019bdd318383
Aukai Tirrell <aukaitirrell@webmocha.com>
2024-11-26 13:36:01 -0700

    update profileicon to use avatar component with next/image

--- src/components/ProfileIcon/ProfileIcon.jsx
+++ src/components/ProfileIcon/ProfileIcon.jsx
@@ -1,9 +1,19 @@
 import Image from "next/image";
+import { Avatar, AvatarImage, AvatarFallback } from "../ui/avatar";
 import { SUPABASE_ASSET_PATH } from "@/lib/constants";
 
 const ProfileIcon = ({ profileIconId }) => {
   const src = `https://${process.env.NEXT_PUBLIC_SUPABASE_ASSET_HOST}${SUPABASE_ASSET_PATH}/icons/profile/${profileIconId}.png`;
-  return <Image priority src={src} alt={profileIconId} width={200} height={200} />;
+  return (
+    <Avatar>
+      <AvatarImage src={src} asChild alt={profileIconId}>
+        <Image priority src={src} alt={profileIconId} height={56} width={56} />
+      </AvatarImage>
+      <AvatarFallback>{profileIconId}</AvatarFallback>
+    </Avatar>
+  );
+
+  // return <Image priority src={src} alt={profileIconId} width={200} height={200} />;
 };
 
 export default ProfileIcon;
