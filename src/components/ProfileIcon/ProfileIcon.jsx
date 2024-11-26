95583ae615168b5e13a0d2f34f68e8bbf0b549be
Aukai Tirrell <aukaitirrell@webmocha.com>
2024-11-26 12:11:20 -0700

    switch to using supabase

--- src/components/ProfileIcon/ProfileIcon.jsx
+++ src/components/ProfileIcon/ProfileIcon.jsx
@@ -1,7 +1,8 @@
 import Image from "next/image";
+import { SUPABASE_ASSET_PATH } from "@/lib/constants";
 
 const ProfileIcon = ({ profileIconId }) => {
-  const src = `https://${process.env.NEXT_PUBLIC_R2_HOST}/profileicons/${profileIconId}.png`;
+  const src = `https://${process.env.NEXT_PUBLIC_SUPABASE_ASSET_HOST}${SUPABASE_ASSET_PATH}/icons/profile/${profileIconId}.png`;
   return <Image priority src={src} alt={profileIconId} width={200} height={200} />;
 };
 
