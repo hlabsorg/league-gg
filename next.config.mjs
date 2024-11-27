c1a606956c9dcb68ebda441f907664e87c538445
Aukai Tirrell <aukaitirrell@webmocha.com>
2024-11-26 18:07:46 -0700

    update config

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        async_hooks: false,
-      };
-    }
-    return config;
-  },
   images: {
     minimumCacheTTL: 7200,
     remotePatterns: [
