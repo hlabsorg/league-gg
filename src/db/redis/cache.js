7c41467552481820e39a9cc7bea8dcd9b563974e
Aukai Tirrell <aukaitirrell@webmocha.com>
2024-12-02 10:26:15 -0700

    normalize redis key

--- src/db/redis/cache.js
+++ src/db/redis/cache.js
@@ -11,15 +11,16 @@ export const setCache = async (
     ex: MAX_AGE,
   },
 ) => {
-  await redis.set(key, JSON.stringify(data), options);
+  await redis.set(key.trim().toLowerCase(), JSON.stringify(data), options);
 };
 
 export const checkCache = async (key) => {
   try {
-    const data = await redis.get(key);
+    const data = await redis.get(key.trim().toLowerCase());
     if (data) {
       // cache hit
       console.log("cache hit: ", key);
+      console.log("DATA", data);
       return data;
     }
     // cache miss;
@@ -31,7 +32,7 @@ export const checkCache = async (key) => {
 
 export const deleteCache = async (key) => {
   try {
-    console.log("deleting cache", key);
+    console.log("deleting cache", key.trim().toLowerCase());
     await redis.del(key);
   } catch (error) {
     console.error(error);
