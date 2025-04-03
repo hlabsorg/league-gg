99df5c5fa0329e568d254eaff28c0e43dede537e
Aukai Tirrell <aukaitirrell@webmocha.com>
2025-04-03 16:06:08 -0600

    update champ mastery

--- src/components/champion-icon.jsx
+++ src/components/champion-icon.jsx
@@ -3,15 +3,14 @@ import { DATA_DRAGON_ASSETS_PATH } from "@/constants/data-dragon";
 
 // const SUPABASE_ASSET_PATH = '/storage/v1/object/public/league-assets';
 
-export function ChampionIcon({ championName, size = 32 }) {
+export function ChampionIcon({ championName, size = 32, className }) {
   return (
     <Image
       src={`${DATA_DRAGON_ASSETS_PATH}/img/champion/${championName}.png`}
       alt={championName}
-      className="size-full object-cover"
+      className={`${className}`}
       width={size}
       height={size}
-      unoptimized
     />
   );
 }
