00b5422a6743d6fc6725d5a40f0e33da13af8031
Aukai Tirrell <aukaitirrell@webmocha.com>
2025-04-03 16:09:24 -0600

    add className to profileicon

--- src/components/profile-icon.jsx
+++ src/components/profile-icon.jsx
@@ -2,10 +2,10 @@ import { Avatar, AvatarImage, AvatarFallback } from "./ui/avatar";
 import { Icons } from "./icons";
 import { DATA_DRAGON_ASSETS_PATH } from "@/constants/data-dragon";
 
-const ProfileIcon = ({ profileIconId }) => {
+const ProfileIcon = ({ profileIconId, className }) => {
   const src = `${DATA_DRAGON_ASSETS_PATH}/img/profileicon/${profileIconId}.png`;
   return (
-    <Avatar>
+    <Avatar className={className}>
       <AvatarImage src={src} alt={profileIconId} />
       <AvatarFallback>
         <Icons.user />
