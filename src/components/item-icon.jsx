4d1260edbdccacc5f01a1e4a94d97b6538525e42
Aukai Tirrell <aukaitirrell@webmocha.com>
2025-04-03 16:29:55 -0600

    add className to item-icon

--- src/components/item-icon.jsx
+++ src/components/item-icon.jsx
@@ -3,17 +3,15 @@ import { DATA_DRAGON_ASSETS_PATH } from "@/constants/data-dragon";
 
 // const SUPABASE_ASSET_PATH = '/storage/v1/object/public/league-assets';
 
-export function ItemIcon({ itemId, size = 24 }) {
+export function ItemIcon({ itemId, size = 24, className }) {
   if (!itemId || itemId === 0) return <div style={{ width: size, height: size }} className="rounded bg-gray-200" />;
-
   return (
     <Image
+      className={`rounded ${className}`}
       src={`${DATA_DRAGON_ASSETS_PATH}/img/item/${itemId}.png`}
       alt={`Item ${itemId}`}
-      className="rounded"
       width={size}
       height={size}
-      unoptimized
     />
   );
 }
