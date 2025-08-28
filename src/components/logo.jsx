12f000c29c69513e7071eae7d2ea30515afded62
Reyn Takahashi <reyntakahashi@Reyns-MacBook-Pro.local>
2025-08-28 14:21:54 -0700

    Post linting/formatting

--- src/components/logo.jsx
+++ src/components/logo.jsx
@@ -4,7 +4,7 @@ export function Logo() {
   return (
     <div className="flex flex-col items-center justify-center">
       <Image src="/assets/elovate_logo.png" alt="Elovate Hero" width={300} height={300} />
-      <h1 className="font-bold text-4xl">Elovate.GG</h1>
+      <h1 className="text-4xl font-bold">Elovate.GG</h1>
     </div>
   );
 }
